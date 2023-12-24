d5e343568e0673da1dd30cbc2c719a372a8b2b23
Efim <120276090+efimandreev0@users.noreply.github.com>
2023-12-24 12:44:15 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlTypes;
+using System.IO;
+using System.Linq;
+using System.Runtime.Remoting.Messaging;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GKHTextTool
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            if (args[0].Contains(".txt"))
+            {
+                Rebuild(args[0], args[1]);
+            }
+            else
+            {
+                Extract(args[0]);
+            }
+        }
+        public static void Extract(string txt)
+        {
+            var reader = new BinaryReader(File.OpenRead(txt));
+            string magic = Encoding.UTF8.GetString(reader.ReadBytes(4));
+            int unk1 = reader.ReadInt32();
+            int unk2 = reader.ReadInt32();
+            int blockSize = unk1 + unk2;
+            reader.BaseStream.Position += 8;
+            int count = reader.ReadInt32();
+            string[] strings = new string[count];
+            reader.BaseStream.Position = Utils.SkipNullBytes(reader) - 1;
+            string textSt = "//TextOffset = " + reader.BaseStream.Position.ToString() + "\n";
+            File.WriteAllText(txt + ".txt", textSt);
+            Console.WriteLine($"{reader.BaseStream.Position}");
+            for (int i = 0; i < count; i++)
+            {
+                strings[i] = Utils.ReadString(reader, Encoding.UTF8);
+                strings[i] = strings[i].Replace("\n", "<lf>").Replace("\r","<br>");
+                File.AppendAllText(txt + ".txt", strings[i] + "\n");
+            }
+        }
+        public static void Rebuild(string text, string file)
+        {
+            var writer = new BinaryWriter(File.OpenWrite(file));
+            writer.BaseStream.Position = 0x20;
+            string[] strings = File.ReadAllLines(text);
+            int offset = int.Parse(Utils.ReadValueAfterEqual(strings[0]));
+            writer.BaseStream.Position = offset;
+            for (int i = 0; i < strings.Length - 1; i++)
+            {
+                if (i == 0)
+                {
+                    continue;
+                }
+                strings[i] = strings[i].Replace("<lf>", "\n").Replace("<br>","\r");
+                writer.Write(Encoding.UTF8.GetBytes(strings[i]));
+                writer.Write(new byte());
+            }
+            int blockSize = 0;
+            while (Utils.IsMultipleOf16((int)writer.BaseStream.Length) == false)
+            {
+                if (Utils.IsMultipleOf16((int)writer.BaseStream.Length) == false)
+                {
+                    writer.Write(new byte());
+                }
+                else
+                {
+                    blockSize = (int)writer.BaseStream.Position - 32;
+                    writer.Write("\u4645\u4F43\u0000\u0000\u1000\u0000\u0000\u0000");
+                }
+            }
+            writer.BaseStream.Position = 4;
+            writer.Write(blockSize);
+            writer.BaseStream.Position += 12;
+            writer.Write(strings.Length - 1);
+        }
+    }
+}
---
+++ Utils.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GKHTextTool
+{
+    internal static class Utils
+    {
+        public static string ReadValueAfterEqual(string input)
+        {
+            int equalIndex = input.IndexOf("="); // Находим индекс символа "="
+
+            if (equalIndex >= 0)
+            {
+                string value = input.Substring(equalIndex + 1).Trim(); // Получаем подстроку после "="
+                return value;
+            }
+
+            return null;
+        }
+
+        public static bool IsMultipleOf16(int number)
+        {
+            return number % 16 == 0;
+        }
+        public static long SkipNullBytes(BinaryReader reader)
+        {
+            long startPosition = reader.BaseStream.Position;
+
+            while (reader.PeekChar() != -1 && reader.ReadByte() == 0x00)
+            {
+                // Пропускаем нулевой байт
+            }
+
+            long endPosition = reader.BaseStream.Position;
+            reader.BaseStream.Position = startPosition; // возвращаем позицию на начало
+            return endPosition;
+        }
+        public static string ReadString(BinaryReader binaryReader, Encoding encoding)
+        {
+            if (encoding == null) throw new ArgumentNullException("encoding");
+
+            List<byte> data = new List<byte>();
+
+            while (binaryReader.BaseStream.Position < binaryReader.BaseStream.Length)
+            {
+                data.Add(binaryReader.ReadByte());
+
+                string partialString = encoding.GetString(data.ToArray(), 0, data.Count);
+
+                if (partialString.Length > 0 && partialString.Last() == '\0')
+                    return encoding.GetString(data.SkipLast(encoding.GetByteCount("\0")).ToArray());
+            }
+            throw new InvalidDataException("Hit end of stream while reading null-terminated string.");
+        }
+        private static IEnumerable<TSource> SkipLast<TSource>(this IEnumerable<TSource> source, int count)
+        {
+            if (source == null) throw new ArgumentNullException("source");
+
+            Queue<TSource> queue = new Queue<TSource>();
+
+            foreach (TSource item in source)
+            {
+                queue.Enqueue(item);
+
+                if (queue.Count > count) yield return queue.Dequeue();
+            }
+        }
+    }
+}
